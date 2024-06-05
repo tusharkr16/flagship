3a315e5842595e37fc5cdc96839a0c29a15cb4ea
tusharkr16 <tusharkumar9871@gmail.com>
2024-06-05 15:02:09 +0530

    created card

---
+++ src/Countries.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    const fetchData = async () => {
+        try {
+            const { data } = await axios.get('https://restcountries.com/v3.1/all');
+            const countriesData = data.map(country => ({
+                name: country.name.common,
+                flag: country.flags.svg
+            }));
+            setCountries(countriesData);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+            {countries.map((country, index) => (
+                <div className='border rounded-lg m-1 mt-5 mb-5 me-3 ms-3' key={index} style={{ flex: '1 0 14%', textAlign: 'center' }}>
+                    <img src={country.flag} alt={`Flag ${index}`} style={{ width: '100%' }} />
+                    <p className='text-xl'>{country.name}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Countries;
